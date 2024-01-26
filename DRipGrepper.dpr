48e1b93c9cff49b03aebdbc713c7fa881b96a6f4
mattia72 <mattiassicha@gmail.com>
2024-01-26 15:12:47 +0100

    dependencies in proj

--- DRipGrepper.dpr
+++ DRipGrepper.dpr
@@ -28,7 +28,25 @@ uses
   u_dzDpiScaleUtils in 'external\dzlib\u_dzDpiScaleUtils.pas',
   u_dzMiscUtils in 'external\dzlib\u_dzMiscUtils.pas',
   u_dzTypesUtils in 'external\dzlib\u_dzTypesUtils.pas',
-  u_dzTypInfo in 'external\dzlib\u_dzTypInfo.pas';
+  u_dzTypInfo in 'external\dzlib\u_dzTypInfo.pas',
+  GX_IdeDock in 'external\GExpert\GX_IdeDock.pas',
+  u_dzVersionInfo in 'external\dzlib\u_dzVersionInfo.pas',
+  u_dzVclUtils in 'external\dzlib\u_dzVclUtils.pas',
+  u_dzVariantUtils in 'external\dzlib\u_dzVariantUtils.pas',
+  u_dzTypes in 'external\dzlib\u_dzTypes.pas',
+  u_dzTranslator in 'external\dzlib\u_dzTranslator.pas',
+  u_dzStringUtils in 'external\dzlib\u_dzStringUtils.pas',
+  u_dzStringArrayUtils in 'external\dzlib\u_dzStringArrayUtils.pas',
+  u_dzSortUtils in 'external\dzlib\u_dzSortUtils.pas',
+  u_dzSortProvider in 'external\dzlib\u_dzSortProvider.pas',
+  u_dzQuicksort in 'external\dzlib\u_dzQuicksort.pas',
+  u_dzOsUtils in 'external\dzlib\u_dzOsUtils.pas',
+  u_dzNamedThread in 'external\dzlib\u_dzNamedThread.pas',
+  u_dzLineBuilder in 'external\dzlib\u_dzLineBuilder.pas',
+  u_dzFileUtils in 'external\dzlib\u_dzFileUtils.pas',
+  u_dzDateUtils in 'external\dzlib\u_dzDateUtils.pas',
+  u_dzConvertUtils in 'external\dzlib\u_dzConvertUtils.pas',
+  u_dzClassUtils in 'external\dzlib\u_dzClassUtils.pas';
 
 {$R *.res}
 
