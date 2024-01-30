e670e9923a48a8e5f8e3e918852e81c8f27b489e
mattia72 <mattiassicha@gmail.com>
2024-01-30 09:13:58 +0100

    unittests ok

--- UnitTest/RipGrepMatchTest.pas
+++ UnitTest/RipGrepMatchTest.pas
@@ -49,26 +49,27 @@ procedure TRipGrepMatchTest.ParseLineTest(const _s : string);
 var
 	m : TVimGrepMatchLineParser;
 begin
-	m.ParseLine(0,_s);
+	m := TVimGrepMatchLineParser.Create();
+	m.ParseLine(0, _s);
 	Assert.IsTrue(not m.ParseResult.IsError,
 		{ } 'Line:' + CRLF +
 		{ } _s + CRLF +
 		{ } m.ParseResult.Columns[Integer(ciFile)].Text + CRLF +
 		{ } m.ParseResult.Columns[Integer(ciRow)].Text + CRLF +
 		{ } m.ParseResult.Columns[Integer(ciCol)].Text + CRLF +
-		{ } m.ParseResult.Columns[Integer(ciText)].Text+ CRLF);
+		{ } m.ParseResult.Columns[Integer(ciText)].Text + CRLF);
 end;
 
 procedure TRipGrepMatchTest.ValidatePathTest(const _s : string);
 var
 	m : TVimGrepMatchLineParser;
 begin
+	m := TVimGrepMatchLineParser.Create();
 	m.ParseLine(0, _s);
 	Assert.IsTrue(not m.ParseResult.IsError,
 		{ } 'Line:' + CRLF +
 		{ } _s + CRLF +
-		{ } m.ParseResult.Columns[Integer(ciFile)].Text
-	);
+		{ } m.ParseResult.Columns[Integer(ciFile)].Text);
 end;
 
 initialization
