6113286372d179e8ae20ead870df752956f90177
mattia72 <mattiassicha@gmail.com>
2024-12-06 15:23:50 +0100

    format simple types unit

--- src/Common/RipGrepper.Common.SimpleTypes.pas
+++ src/Common/RipGrepper.Common.SimpleTypes.pas
@@ -3,13 +3,14 @@ unit RipGrepper.Common.SimpleTypes;
 interface
 
 uses
-	System.Classes, System.SysUtils;
+	System.Classes,
+	System.SysUtils;
 
 type
 	TParserType = (ptEmpty, ptRipGrepSearch, ptRipGrepPrettySearch, ptRipGrepStats, ptRipGrepVersion, ptRipGrepError, ptRipGrepHelp);
 	// TODO : use this instead of IsError IsStats
 	EParsedLineType = (pltErrorLine, pltStatsLine, pltContexLine);
-	TParsedLineType  = set of  EParsedLineType;
+	TParsedLineType = set of EParsedLineType;
 
 	TFileNameType = (ftAbsolute, ftRelative);
 
@@ -20,20 +21,19 @@ type
 	EFilterMode = (fmFilterFile, fmFilterText, fmCaseSensitive, fmUseRegex);
 	{$SCOPEDENUMS OFF}
 	TGuiReplaceModes = set of EGuiReplaceMode;
-    TFilterModes = set of EFilterMode;
+	TFilterModes = set of EFilterMode;
 
-    ESkipFileReplaceException = class(Exception);
-    EFileOpenException = class(Exception);
+	ESkipFileReplaceException = class(Exception);
+	EFileOpenException = class(Exception);
 
 	TErrorCounters = record
 		FSumOfErrors : Integer;
 		FParserErrors : Integer;
-        FStatLineCount : Integer;
+		FStatLineCount : Integer;
 		FIsNoOutputError : Boolean;
 		FIsRGReportedError : Boolean;
 		procedure Reset;
 	end;
-
 
 implementation
 
@@ -41,7 +41,7 @@ procedure TErrorCounters.Reset;
 begin
 	FSumOfErrors := 0;
 	FParserErrors := 0;
-    FStatLineCount := 0;
+	FStatLineCount := 0;
 	FIsNoOutputError := False;
 	FIsRGReportedError := False;
 end;
