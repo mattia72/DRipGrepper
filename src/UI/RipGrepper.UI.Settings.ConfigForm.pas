c484ac75830571951fe980ce1aee3d456f991551
mattia72 <mattiassicha@gmail.com>
2024-12-10 13:11:21 +0100

    config form create hourglas

--- src/UI/RipGrepper.UI.Settings.ConfigForm.pas
+++ src/UI/RipGrepper.UI.Settings.ConfigForm.pas
@@ -71,30 +71,35 @@ constructor TConfigForm.Create(_settings : TRipGrepperSettings);
 begin
 	inherited Create(nil);
 	Settings := _settings;
-	// write ini file content
-	Settings.UpdateIniFile;
+	Screen.Cursor := crHourGlass;
+	try
+		// write ini file content
+		Settings.UpdateIniFile;
 
-	FOpenWithConfigForm := TOpenWithConfigForm.Create(nil, Settings.OpenWithSettings);
-	FOpenWithConfigForm.Caption := 'Open With...';
-	FAppSettingsForm := TAppSettingsForm.Create(nil, Settings);
-	FColorSettingsForm := TColorSettingsForm.Create(nil, Settings.FontColorSettings);
+		FOpenWithConfigForm := TOpenWithConfigForm.Create(nil, Settings.OpenWithSettings);
+		FOpenWithConfigForm.Caption := 'Open With...';
+		FAppSettingsForm := TAppSettingsForm.Create(nil, Settings);
+		FColorSettingsForm := TColorSettingsForm.Create(nil, Settings.FontColorSettings);
 
-	FExtensionSettings := TRipGrepperExtensionSettings.Create(Settings.IniFile);
-	FExtensionSettings.ReadIni;
-	FExtensionSettings.LoadFromDict;
-	FExtensionSettingsForm := TExtensionSettingsForm.Create(nil, FExtensionSettings);
+		FExtensionSettings := TRipGrepperExtensionSettings.Create(Settings.IniFile);
+		FExtensionSettings.ReadIni;
+		FExtensionSettings.LoadFromDict;
+		FExtensionSettingsForm := TExtensionSettingsForm.Create(nil, FExtensionSettings);
 
-	FSettingsForms := TObjectList<TForm>.Create();
-	FSettingsForms.AddRange([
-		{ } FAppSettingsForm,
-		{ } FColorSettingsForm,
-		{ } FOpenWithConfigForm,
-		{ } FExtensionSettingsForm]);
+		FSettingsForms := TObjectList<TForm>.Create();
+		FSettingsForms.AddRange([
+			{ } FAppSettingsForm,
+			{ } FColorSettingsForm,
+			{ } FOpenWithConfigForm,
+			{ } FExtensionSettingsForm]);
+	finally
+		Screen.Cursor := crDefault;
+	end;
 end;
 
 destructor TConfigForm.Destroy;
 begin
-    FExtensionSettings.Free;
+	FExtensionSettings.Free;
 	FSettingsForms.Free;
 	Settings.ReCreateMemIni;
 	Settings.ReLoad;
@@ -125,20 +130,25 @@ var
 	iMaxHeight : integer;
 	iMaxWidth : integer;
 begin
-	iMaxHeight := 0;
-	iMaxWidth := 0;
-	for var form : TForm in FSettingsForms do begin
-		iMaxHeight := System.Math.Max(iMaxHeight, form.Height);
-		iMaxWidth := System.Math.Max(iMaxWidth, form.Width);
-		form.ManualDock(PageControl1);
-		form.Show();
+	Screen.Cursor := crHourGlass;
+	try
+		iMaxHeight := 0;
+		iMaxWidth := 0;
+		for var form : TForm in FSettingsForms do begin
+			iMaxHeight := System.Math.Max(iMaxHeight, form.Height);
+			iMaxWidth := System.Math.Max(iMaxWidth, form.Width);
+			form.ManualDock(PageControl1);
+			form.Show();
+		end;
+		FOpenWithConfigForm.pnlBottom.Visible := False;
+		self.Height := iMaxHeight + PageControl1.TabHeight + pnlBottom.Height;
+		self.Width := iMaxWidth; // TODO
+//		Autoscroll := true;
+//		VertScrollBar.Range := iMaxHeight;
+		PageControl1.TabIndex := 0;
+	finally
+		Screen.Cursor := crDefault;
 	end;
-	FOpenWithConfigForm.pnlBottom.Visible := False;
-	self.Height := iMaxHeight + PageControl1.TabHeight;
-	self.Width := iMaxWidth; // TODO
-	// Autoscroll := true;
-	// VertScrollBar.Range := iMaxHeight;
-	PageControl1.TabIndex := 0;
 end;
 
 end.
