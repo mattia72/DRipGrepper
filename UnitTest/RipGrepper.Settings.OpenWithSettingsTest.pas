5c8f686eb704e7fede7b107f839aeec7081d52c0
mattia72 <mattiassicha.tmp@gmail.com>
2025-03-15 19:12:38 +0100

    OpenWithSettings tests

--- UnitTest/RipGrepper.Settings.OpenWithSettingsTest.pas
+++ UnitTest/RipGrepper.Settings.OpenWithSettingsTest.pas
@@ -16,40 +16,59 @@ type
 			procedure CommandListShouldBeFilledWithDefaults();
 			[Test]
 			procedure CommandListShouldBeFilledInDict();
+			[Test]
+			procedure CommandListShouldBePersisted();
 	end;
 
 implementation
 
 uses
 	RipGrepper.Settings.SettingsDictionary,
-    RipGrepper.OpenWith.Constants;
+	RipGrepper.OpenWith.Constants,
+	System.SysUtils;
 
 procedure TInnerOpenWithSettingsTest.CommandListShouldBeFilledWithDefaults();
 begin
 	var
 	dbgArr := TSettingsDictionary.DictToStringArray(FSettings.SettingsDict());
 
-    var arr := FSettings.OpenWithSettings.GetCommands;
+	var
+	arr := FSettings.OpenWithSettings.GetCommands;
 	Assert.AreEqual(Length(arr), Length(DEFAULT_EDITORS), 'Default Editors should be equal');
 end;
 
 procedure TInnerOpenWithSettingsTest.CommandListShouldBeFilledInDict();
 var
-	dictVal: string;
-	settingVal: string;
+	dictVal : string;
+	settingVal : string;
 begin
-	var
-	dbgArr := TSettingsDictionary.DictToStringArray(FSettings.SettingsDict());
+	for var i := 0 to FSettings.OpenWithSettings.Count do begin
+		var
+		key := Format('%s%d', [OPENWITH_COMMAND_KEY, i]);
 
-	settingVal := FSettings.OpenWithSettings.Command[0];
-	dictVal := FSettings.SettingsDict()[FSettings.OpenWithSettings.IniSectionName]['Command_0'].AsString;
+		settingVal := FSettings.OpenWithSettings.Command[i];
+		dictVal := FSettings.SettingsDict()[FSettings.OpenWithSettings.IniSectionName][key].AsString;
 
-	Assert.AreEqual(settingVal, dictVal, 'Command 0 should be equal');
+		Assert.AreEqual(settingVal, dictVal, 'Command 0 should be equal');
+	end;
 end;
 
-
-// iniVal := FFactory.GetStringPersister(FSettings.SearchFormSettings.IniSectionName, 'Encoding').LoadFromFile;
-// settingVal := FSettings.SearchFormSettings.Encoding;
+procedure TInnerOpenWithSettingsTest.CommandListShouldBePersisted();
+var
+	iniVal : string;
+	settingVal : string;
+	i : integer;
+begin
+	FSettings.UpdateIniFile();
+	i := 0;
+	for settingVal in FSettings.OpenWithSettings.GetCommands do begin
+		var
+		key := Format('%s%d', [OPENWITH_COMMAND_KEY, i]);
+		iniVal := FFactory.GetStringPersister(FSettings.OpenWithSettings.IniSectionName, key).LoadFromFile;
+		Assert.AreEqual(settingVal, iniVal, key + ' should be persisted.');
+		Inc(i);
+	end;
+end;
 
 initialization
 
