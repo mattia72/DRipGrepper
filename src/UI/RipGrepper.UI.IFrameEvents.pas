91ce64c7d44f0bcf84dfdceaa750ab5e1417f976
mattia72 <mattiassicha.tmp@gmail.com>
2025-04-28 21:28:28 +0200

    some new todos

--- src/UI/RipGrepper.UI.IFrameEvents.pas
+++ src/UI/RipGrepper.UI.IFrameEvents.pas
@@ -3,13 +3,13 @@ unit RipGrepper.UI.IFrameEvents;
 interface
 
 type
-	IFrameEvents = interface
+	IFrameEvents = interface                       // TODO: rename IFrameEventHandler
 		['{4E501DFF-A887-4EF3-A376-9A177EE499FE}']
 		procedure Init;
 		procedure BeforeSearch(var _bAbort : Boolean);
 		procedure AfterSearch;
 		procedure AfterHistObjChange;
-		procedure UpdateUIStyle(_sNewStyle : string = '');
+		procedure UpdateUIStyle(_sNewStyle : string = '');   // TODO: it should be cleaned
 	end;
 
 implementation
