babefc9c307865d5cbdacd2f99e5f0491a0f52fe
mattia72 <mattiassicha@gmail.com>
2026-02-06 07:40:46 +0100

    formatting

--- src/Helper/RipGrepper.Helper.SettingStoreBehaviours.pas
+++ src/Helper/RipGrepper.Helper.SettingStoreBehaviours.pas
@@ -15,49 +15,49 @@ type
 
 	TSettingStoreBehavioursHelper = class
 		const
-			SettingStoreBehaviourNames: array [TSettingStoreBehaviour] of string = (
-        { } 'ssbNotSet', 
-				{ } 'ssbStoreIfModified', 
-        { } 'ssbStoreAfterChangeImmediately', 
+			SettingStoreBehaviourNames : array [TSettingStoreBehaviour] of string = (
+				{ } 'ssbNotSet',
+				{ } 'ssbStoreIfModified',
+				{ } 'ssbStoreAfterChangeImmediately',
 				{ } 'ssbStoreOnceEvenIfNotModified');
 
 		private
-			FBehaviours: TSettingStoreBehaviours;
+			FBehaviours : TSettingStoreBehaviours;
 
 		public
-			constructor Create(const _behaviours: TSettingStoreBehaviours);
-			class function ToString(const _behaviours: TSettingStoreBehaviours): string; reintroduce;
-			class function FromString(const _value: string): TSettingStoreBehaviours;
+			constructor Create(const _behaviours : TSettingStoreBehaviours);
+			class function ToString(const _behaviours : TSettingStoreBehaviours) : string; reintroduce;
+			class function FromString(const _value : string) : TSettingStoreBehaviours;
 
-			property Behaviours: TSettingStoreBehaviours read FBehaviours write FBehaviours;
+			property Behaviours : TSettingStoreBehaviours read FBehaviours write FBehaviours;
 	end;
 
 implementation
 
-constructor TSettingStoreBehavioursHelper.Create(const _behaviours: TSettingStoreBehaviours);
+constructor TSettingStoreBehavioursHelper.Create(const _behaviours : TSettingStoreBehaviours);
 begin
 	FBehaviours := _behaviours;
 end;
 
-class function TSettingStoreBehavioursHelper.ToString(const _behaviours: TSettingStoreBehaviours): string;
+class function TSettingStoreBehavioursHelper.ToString(const _behaviours : TSettingStoreBehaviours) : string;
 var
-	behaviourNames: TArray<string>;
+	behaviourNames : TArray<string>;
 begin
 	behaviourNames := [];
-	for var behaviour: TSettingStoreBehaviour in _behaviours do begin
+	for var behaviour : TSettingStoreBehaviour in _behaviours do begin
 		behaviourNames := behaviourNames + [SettingStoreBehaviourNames[behaviour]];
 	end;
 	Result := string.Join(',', behaviourNames);
 end;
 
-class function TSettingStoreBehavioursHelper.FromString(const _value: string): TSettingStoreBehaviours;
+class function TSettingStoreBehavioursHelper.FromString(const _value : string) : TSettingStoreBehaviours;
 var
-	behaviourNames: TArray<string>;
+	behaviourNames : TArray<string>;
 begin
 	Result := [];
 	behaviourNames := _value.Split([',']);
-	for var behaviourName: string in behaviourNames do begin
-		for var behaviour: TSettingStoreBehaviour := low(TSettingStoreBehaviour) to high(TSettingStoreBehaviour) do begin
+	for var behaviourName : string in behaviourNames do begin
+		for var behaviour : TSettingStoreBehaviour := low(TSettingStoreBehaviour) to high(TSettingStoreBehaviour) do begin
 			if SettingStoreBehaviourNames[behaviour] = behaviourName then begin
 				Include(Result, behaviour);
 			end;
